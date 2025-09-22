016e05605a04bc5cc638a0d705f8a266afe65527
roberttidey <robert@tideys.co.uk>
2025-09-22 13:50:42 +0100

    Resync with powershell (extra fields added)

--- python/GE-DaysFullDataPoints.py
+++ python/GE-DaysFullDataPoints.py
@@ -13,8 +13,8 @@ DatePick = DateFirst
 DateCount = int(input("Enter number of days : "))
 pageX = 1
 pageSizeX = 1000
-DataPointsStr = "Date,Solar,Import,Export,Consumption,Battery\n"
-## If PeriodInterval is 0 then IntervalTimes are used else it sets a regular interval throughout the day e.g. 30 minutes
+DataPointsStr = "Date,Solar Today,Solar Total,Import Today,Import Total,Export Today,Export Total,Consumption Today,Consumption Total,Charge Today,Discharge Today,Battery SoC %\n"
+
 TWOPLACES = Decimal('0.01')
 
 def WriteDateFullData():
@@ -24,14 +24,20 @@ def WriteDateFullData():
   response = requests.get(url, headers = headers)
   Data = response.json()['data']
   last = len(Data)
-  parArray = ["","","","","",""]
+  parArray = ["","","","","","","","","","","",""]
   for rec in range(last):
     parArray[0] = Data[rec]['time']
     parArray[1] = str(Decimal(Data[rec]['today']['solar']).quantize(TWOPLACES))
-    parArray[2] = str(Decimal(Data[rec]['today']['grid']['import']).quantize(TWOPLACES))
-    parArray[3] = str(Decimal(Data[rec]['today']['grid']['export']).quantize(TWOPLACES))
-    parArray[4] = str(Decimal(Data[rec]['today']['consumption']).quantize(TWOPLACES))
-    parArray[5] = str(int(Data[rec]['power']['battery']['percent']))
+    parArray[2] = str(Decimal(Data[rec]['total']['solar']).quantize(TWOPLACES))
+    parArray[3] = str(Decimal(Data[rec]['today']['grid']['import']).quantize(TWOPLACES))
+    parArray[4] = str(Decimal(Data[rec]['total']['grid']['import']).quantize(TWOPLACES))
+    parArray[5] = str(Decimal(Data[rec]['today']['grid']['export']).quantize(TWOPLACES))
+    parArray[6] = str(Decimal(Data[rec]['total']['grid']['export']).quantize(TWOPLACES))
+    parArray[7] = str(Decimal(Data[rec]['today']['consumption']).quantize(TWOPLACES))
+    parArray[8] = str(Decimal(Data[rec]['total']['consumption']).quantize(TWOPLACES))
+    parArray[9] = str(Decimal(Data[rec]['today']['battery']['charge']).quantize(TWOPLACES))
+    parArray[10] = str(Decimal(Data[rec]['today']['battery']['discharge']).quantize(TWOPLACES))
+    parArray[11] = str(int(Data[rec]['power']['battery']['percent']))
     DataPointsStr = ','.join(parArray) + "\n"
     with open(datafilename, 'a') as datafile:
       datafile.write(DataPointsStr)
